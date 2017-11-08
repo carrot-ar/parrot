9acf88e10275d855886d46ad6fb7fd1b9cbc331c
gonzalonunez <gonzi@tcpmiami.com>
2017-11-07 20:59:38 -0500

    Added tests for BeaconParams initializer

--- ParrotTests/ParrotTests.swift
+++ ParrotTests/ParrotTests.swift
@@ -9,28 +9,31 @@
 import XCTest
 @testable import Parrot
 
+import CoreLocation
+
 class ParrotTests: XCTestCase {
-    
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-    
+  
+  let uuid = UUID(uuidString: "E621E1F8-C36C-495A-93FC-0C247A3E6E5F")!
+  let identifier = "com.ParrotTests.Beacon"
+  
+  func testNoParams() {
+    let beacon = CLBeaconRegion(uuid: uuid, identifier: identifier, params: .none)
+    assert(beacon.identifier == identifier)
+    assert(beacon.major == nil)
+    assert(beacon.minor == nil)
+  }
+  
+  func testOnlyMajorParams() {
+    let beacon = CLBeaconRegion(uuid: uuid, identifier: identifier, params: .major(100))
+    assert(beacon.identifier == identifier)
+    assert(beacon.major == 100)
+    assert(beacon.minor == nil)
+  }
+  
+  func testBothMajorAndMinorParams() {
+    let beacon = CLBeaconRegion(uuid: uuid, identifier: identifier, params: .both(major: 100, minor: 50))
+    assert(beacon.identifier == identifier)
+    assert(beacon.major == 100)
+    assert(beacon.minor == 50)
+  }
 }
